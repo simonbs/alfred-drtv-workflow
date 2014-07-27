32449f3826508946b8a6c9665606a53024ada6fb
Simon Støvring <simonstoevring@gmail.com>
2014-07-27 17:49:20 +0200

    Updates workflow to support the new changes in Danmarks Radios API

--- workflow/DanmarksRadio.py
+++ workflow/DanmarksRadio.py
@@ -15,7 +15,7 @@ class DanmarksRadio(object):
 			term = '%s*' % (term)
 		path = 'Search/Bundle?Title=$like(\'%s\')' % urllib2.quote(term)
 		return self.__load_path(path)
-		
+
 	def search_parsed(self, term, load_program_cards = False):
 		'''Search DR TV for shows and return results as instances of DanmarksRadioSearchResult'''
 		response = self.search(term)
@@ -23,7 +23,7 @@ class DanmarksRadio(object):
 		if response is not None:
 			items = response['Data']
 			for item in items:
-				if 'Slug' in item and 'Title' in item and 'Relations' in item:					
+				if 'Slug' in item and 'Title' in item and 'Relations' in item:
 					# Check if we can find an image for the program
 					image_url = None
 					if 'Assets' in item:
@@ -40,12 +40,12 @@ class DanmarksRadio(object):
 					)
 					results.append(result)
 		return results
-		
+
 	def program_cards(self, slug):
 		'''Loads the program information for the specified slug'''
 		path = 'programcard?Relations.Slug=$eq(\'%s\')&limit=$eq(100)' % urllib2.quote(slug)
 		return self.__load_path(path)
-		
+
 	def program_cards_parsed(self, slug):
 		'''Loads the program information for the specified slug as instances of DanmarksRadioProgramCard'''
 		response = self.program_cards(slug)
@@ -57,12 +57,12 @@ class DanmarksRadio(object):
 				if card is not None:
 					cards.append(card)
 		return cards
-		
+
 	def most_viewed(self, start_date, timespan):
 		'''Loads the most viewed programs for a period'''
 		path = 'ProgramViews/MostViewed?from=%s&span=%s' % (start_date, timespan)
 		return self.__load_path(path)
-		
+
 	def most_viewed_parsed(self, start_date, timespan):
 		'''Loads the most viewed programs and parses them into instances of DanmarksRadioProgramCard'''
 		response = self.most_viewed(start_date, timespan)
@@ -76,29 +76,26 @@ class DanmarksRadio(object):
 							card.total_views = item['TotalViews']
 						cards.append(card)
 		return cards
-		
+
 	def most_viewed_this_week(self):
 		'''Loads the most viewed programs for this week'''
 		today = date.today()
 		previous_week = today + relativedelta(weeks = -1)
 		timespan = '%s:00:00:00' % ((today - previous_week).days)
 		return self.most_viewed_parsed(previous_week, timespan)
-		
+
 	def get_direct_video_url(self, video_url):
 		'''Gives a direct video URL'''
 		response = self.__load_url(video_url)
 		if response is not None and 'Links' in response:
 			links = response['Links']
-			ios_links = []
+			download_link = None
 			for link in links:
-				if link['Target'] == 'Ios': # We are interested in finding links which are viewable on an iOS device
-					ios_links.append(link)
-			if len(ios_links) > 0:
-				# We sort the list to find the link with the highest bitrate
-				sorted_links = sorted(ios_links, key = operator.itemgetter('Bitrate'), reverse = True)
-				return sorted_links[0]['Uri']
-		return None
-		
+				if link['Target'] == 'HLS' and link['FileFormat'] == 'mp4':
+					download_link = link['Uri']
+					break
+		return download_link
+
 	def __parse_program_card(self, item):
 		'''Parses a program card item'''
 		if 'Slug' in item and 'Title' in item and 'Relations' in item:
@@ -123,7 +120,7 @@ class DanmarksRadio(object):
 				broadcast_time_str = item['PrimaryBroadcastStartTime']
 				regex_milliseconds = r'^\d{4}-\d{1,2}-\d{1,2}T\d{1,2}:\d{1,2}:\d{1,2}\.\d{1,4}Z$'
 				regex_no_milliseconds = r'^\d{4}-\d{1,2}-\d{1,2}T\d{1,2}:\d{1,2}:\d{1,2}Z$'
-				if re.compile(regex_no_milliseconds).match(broadcast_time_str) is not None:	
+				if re.compile(regex_no_milliseconds).match(broadcast_time_str) is not None:
 					broadcast_time = datetime.strptime(broadcast_time_str, '%Y-%m-%dT%H:%M:%SZ')
 				elif re.compile(regex_milliseconds).match(broadcast_time_str) is not None:
 					broadcast_time = datetime.strptime(broadcast_time_str, '%Y-%m-%dT%H:%M:%S.%fZ')
@@ -143,12 +140,12 @@ class DanmarksRadio(object):
 				video_url
 			)
 			return card
-		
+
 	def __load_path(self, path):
 		'''Load and parse the response of a request'''
 		url = 'http://www.dr.dk/mu/%s' % path
 		return self.__load_url(url)
-			
+
 	def __load_url(self, url):
 		'''Load and parse the response of a request'''
 		response = urllib2.urlopen(url)
@@ -157,30 +154,30 @@ class DanmarksRadio(object):
 			return json.loads(contents)
 		except ValueError, e:
 			return None
-	
+
 	def __safe_value(self, key, pair):
 		'''Safely retrieves the value for the specified key in the specified dictionary'''
 		return pair[key] if key in pair else None
-		
+
 class DanmarksRadioProgram(object):
 	def __init__(self, slug, title, genre, image_url):
 		self.__slug = slug
 		self.__title = title
 		self.__genre = genre
 		self.__image_url = image_url
-		
+
 	@property
 	def image_url(self):
 		return self.__image_url
-		
+
 	@property
 	def slug(self):
 		return self.__slug
-		
+
 	@property
 	def title(self):
 		return self.__title
-		
+
 	@property
 	def genre(self):
 		return self.__genre
@@ -189,15 +186,15 @@ class DanmarksRadioSearchResult(DanmarksRadioProgram):
 	def __init__(self, slug, title, genre, image_url, video_url, program_cards = None):
 		super(self.__class__, self).__init__(slug, title, genre, image_url)
 		self.__program_cards = program_cards
-		
+
 	@property
 	def program_cards(self):
 		return self.__program_cards
-		
+
 	@property
 	def page_url(self):
 		return 'http://www.dr.dk/tv/se/%s' % (self.slug)
-		
+
 class DanmarksRadioProgramCard(DanmarksRadioProgram):
 	def __init__(self, program_slug, slug, title, genre, description, broadcast_time, image_url, video_url):
 		super(self.__class__, self).__init__(slug, title, genre, image_url)
@@ -206,28 +203,27 @@ class DanmarksRadioProgramCard(DanmarksRadioProgram):
 		self.__broadcast_time = broadcast_time
 		self.__video_url = video_url
 		self.total_views = -1
-		
+
 	@property
 	def program_slug(self):
 		return self.__program_slug
-		
+
 	@property
 	def description(self):
 		return self.__description
-		
+
 	@property
 	def broadcast_time(self):
 		return self.__broadcast_time
-		
+
 	@property
 	def video_url(self):
 		return self.__video_url
-		
+
 	@property
 	def page_url(self):
 		return 'http://www.dr.dk/tv/se/%s/%s' % (self.program_slug, self.slug)
-		
+
 	@property
 	def is_available(self):
 		return self.__video_url != None
-	
